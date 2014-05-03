997ce07820bf31d4dbf3dd47f20a7b98d029a75a
Bill Schwanitz <bilsch@bilsch.org>
2014-05-03 16:52:21 -0400

    sync
    repetier is dead to me now ;) keeping the configs just in case
    
    been playing a *lot* with different filaments. Also added .1mm layer
    heights. Wow ;) Other than some voids in the shell only treatment
    is removing the brim ;)

--- sync.sh
+++ sync.sh
@@ -4,8 +4,8 @@ set -xv
 
 rsync  -av ~/Library/Application*Support/Slic3r/ slic3r/
 cp ../Marlin-orig/Marlin/Configuration.h marlin/
-cp ~/Library/Repetier/slic3r.ini repetier_host/
-cp -a /Applications/*.ini kisslicer/
+#cp ~/Library/Repetier/slic3r.ini repetier_host/
+#cp -a /Applications/*.ini kisslicer/
 
 git add -A
 git commit
